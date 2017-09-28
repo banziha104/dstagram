38d1ace8c936861af61d5cebb5835a4c9f532554
iyeongjun <they6687@naver.com>
2017-09-28 16:53:10 +0900

    Add

--- photo/admin.py
+++ photo/admin.py
@@ -8,3 +8,8 @@ class PhotoAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)                        # 회원번호 등에서 쓰듯, 검색 가능,은 필수
     ordering = ['updated','created']                   # 모델의 odering과 다름, admin페이지의 오더링순
 admin.site.register(Photo,PhotoAdmin)
+
+
+class A:
+    def funcA(self):
+        print(self)
